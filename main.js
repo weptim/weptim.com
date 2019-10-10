f6e60716ae8bef625211e816a756101c8cb0e04f
weptim <weptimug@gmail.com>
2019-10-10 12:03:27 +0300

    commit all

---
+++ main.js
@@ -0,0 +1,6 @@
+let showMenu = document.querySelector("#js-toggle-menu");
+showMenu.addEventListener("click", runEvent);
+function runEvent(){
+	alert(12);
+var menu = document.querySelector(".menu-list");
+menu.style.display = "block";
